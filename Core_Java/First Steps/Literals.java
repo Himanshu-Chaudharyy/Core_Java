77f22666676c7acc07d787164e00c61944e820d8
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-04 11:13:27 +0530

    Add files via upload

---
+++ Core_Java/First Steps/Literals.java
@@ -0,0 +1,17 @@
+public class Literals {
+    public static void main(String[]args){
+        byte age=25;
+        int age2=26;
+        short age3=27;
+        long phonenumber=9999999999L;
+        char ch='A';
+        float f=5.6f;
+        double d1=4.44;
+        boolean a=true;
+        System.out.print(age);
+        String str="Himanshu";
+        System.out.println(str);
+
+
+    }
+}
