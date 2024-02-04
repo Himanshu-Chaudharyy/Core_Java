141313c428f7aa686085b35b7fe155f83652176f
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-04 19:35:46 +0530

    Add files via upload

---
+++ Core_Java/First Steps/Control Flow/DecisionMaking.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class DecisionMaking {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age to check you can vote or not :");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("You can vote");
+            }
+        else{
+            System.out.println("You can't vote");
+        }
+}
+}
