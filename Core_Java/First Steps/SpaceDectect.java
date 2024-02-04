d50defd71eb570ca1c41f99a930b49308ace56e1
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-04 19:14:51 +0530

    Add files via upload

---
+++ Core_Java/First Steps/SpaceDectect.java
@@ -0,0 +1,7 @@
+public class SpaceDectect {
+    public static void main(String[]args){
+        String name="My name     is Himanshu";
+        System.out.println(name.indexOf("   "));
+    }
+    
+}
