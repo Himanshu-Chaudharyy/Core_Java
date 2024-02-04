4a8fbe115d71f6f085ea0e01c0398c554190b15f
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-04 21:08:31 +0530

    Add files via upload

---
+++ Control Flow/ValidUsernameAndPassword.java
@@ -0,0 +1,13 @@
+public class ValidUsernameAndPassword {
+    public static void main(String[]args){
+        int id=12345;
+        int pass=12345;
+        if(id==12345&&pass==12345){
+            System.out.println("Login Successful");
+        }
+        else{
+            System.out.println("Pass or id is wrong");
+        }
+    }
+    
+}
