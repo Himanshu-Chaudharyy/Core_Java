2961b14d9194cca18bf3dd5f9d5b1108b8820f23
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-04 17:46:29 +0530

    Add files via upload

---
+++ Core_Java/First Steps/EncripDecriptGrade.java
@@ -0,0 +1,12 @@
+public class EncripDecriptGrade {
+    public static void main(String[] args) {
+        char grade = 'B';
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+
+        // decript grade
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+    }
+
+}
