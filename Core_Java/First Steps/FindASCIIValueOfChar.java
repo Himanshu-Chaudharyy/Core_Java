75553c31a06394c7d67bdb3f14d738259bede430
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-04 16:47:53 +0530

    Add files via upload

---
+++ Core_Java/First Steps/FindASCIIValueOfChar.java
@@ -0,0 +1,8 @@
+public class FindASCIIValueOfChar {
+    public static void main(String[]args){
+        char ch='A';
+        int ascii=(int)ch;//cast char to int
+
+        System.out.println("The ASCII value of "+ch+" is :"+ascii);
+    }
+}
