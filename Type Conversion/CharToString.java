ac9654839df1f105f18f559924e036e847a862d6
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-06 10:53:37 +0530

    Add files via upload

---
+++ Type Conversion/CharToString.java
@@ -0,0 +1,11 @@
+public class CharToString1 {
+    public static void main(String[]args){
+        char ch = 'a';
+        String st = String.valueOf(ch);
+        
+        // or simply
+        // String st = "" + ch;
+        System.out.println(st);
+        
+    }
+}
