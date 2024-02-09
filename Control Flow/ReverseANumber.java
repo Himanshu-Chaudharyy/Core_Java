7d0a3dd43a66985d104f82322cf8fc93bacd7aeb
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-09 22:14:41 +0530

    Add files via upload

---
+++ Control Flow/ReverseANumber.java
@@ -0,0 +1,8 @@
+public class ReverseANumber {
+    public static void main(String []args){
+        int num=1;
+        for (num=1;num>=10;--num){
+            System.out.println(num);
+        }
+    }
+}
