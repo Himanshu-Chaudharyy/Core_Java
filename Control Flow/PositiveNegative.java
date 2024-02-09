2bb72bcc905c5fa576c3740e1eb07e88f4000c33
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-09 22:14:39 +0530

    Add files via upload

--- Control Flow/PositiveNegative.java
+++ Control Flow/PositiveNegative.java
@@ -1,18 +1,17 @@
+import java.util.Scanner;
+
 public class PositiveNegative {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
 
-        double number = 12.3;
-
-        // true if number is less than 0
-        if (number < 0.0)
-            System.out.println(number + " is a negative number.");
-
-        // true if number is greater than 0
-        else if ( number > 0.0)
-            System.out.println(number + " is a positive number.");
-
-        // if both test expression is evaluated to false
-        else
-            System.out.println(number + " is 0.");
+        if (number > 0) {
+            System.out.println(number + " is positive.");
+        } else if (number < 0) {
+            System.out.println(number + " is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
     }
 }
