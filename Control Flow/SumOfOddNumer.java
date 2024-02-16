680434c457e926d229e446466415c7bff68f15d3
Himanshu-Chaudharyy <116719238+Himanshu-Chaudharyy@users.noreply.github.com>
2024-02-16 21:59:59 +0530

    Update SumOfOddNumer.java

--- Control Flow/SumOfOddNumer.java
+++ Control Flow/SumOfOddNumer.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SumOfOddNumer {
+public class SumOfOddNumer{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter sum of odd number range to fun sum :");
